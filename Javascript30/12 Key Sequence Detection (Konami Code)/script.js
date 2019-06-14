fa9baf931a76a4f597b534e4a399d5ae06d17a63
Sylwester Karbowniczek <47239638+wczorajszy588@users.noreply.github.com>
2019-06-14 10:51:46 +0200

    Reorganizing code logic.
    I've changed functions to be more atomic and gave them more accurate names.

--- Javascript30/12 Key Sequence Detection (Konami Code)/script.js
+++ Javascript30/12 Key Sequence Detection (Konami Code)/script.js
@@ -1,36 +1,40 @@
 window.addEventListener("load", handleSite);
 
 function handleSite() {
-    window.addEventListener("keydown", addKey);
+    window.addEventListener("keydown", afterKeyFlow);
     const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
     var mySequence = [];
     var outOfTime; //for timeOut function Id
     const iconsFromPage = document.querySelectorAll(".key");
-    function addKey(event) {
+
+    updateMessage("Wpisz kod na nieśmiertelność.");
+    updateTip("Musisz być szybki.");
+
+    function afterKeyFlow(event) {
         clearTimeout(outOfTime); //cancel previous sequence reseting
         outOfTime = setTimeout(resetSequence, 250); //if is 0.25 sec gap between keys, sequence will be reseted
-        document.querySelector(".message").textContent = "Wpisz kod na nieśmiertelność."
-        document.querySelector(".wrapper").classList.remove("immortal");
-        mySequence.push(event.keyCode);
-
-        markProgress();
-        if ( checkForKonamiCode() ) {
-            clearTimeout(outOfTime); //to stop reseting after success
-            document.querySelector(".wrapper").classList.add("immortal");
-            document.querySelector(".message").textContent = "Udało się! Jesteś nieśmiertelny."
-        }
+        addKeyToSequence(event.keyCode);
+        markValidSequence();
+        checkForKonamiCode() ? success() : 0;         //if whole sequence successed
     }
-    //marking progress on site
-    function markProgress() {
-        mySequence.map(function (key, index) {
+    //function backs elements to initial value after success
+    function setInitialState() {
+        window.removeEventListener("keydown", setInitialState);
+        document.querySelector(".wrapper").classList.remove("immortal");
+        updateMessage("Spróbój jeszcze raz.");
+        updateTip("Teraz powinno być łatwiej.");
+    }
+    //turning buttons that match sequence into red
+    function markValidSequence() {
+        mySequence.every(function (key, index) {
             if (key == konamiCode[index]) {
                 setIconAsGood(iconsFromPage[index]);
+                return true;
             }
             else {
-                resetSequence();
+                return false;
             }
-            return key;
-        })
+        });
     }
     //check if sequention is whole and valid
     function checkForKonamiCode() {
@@ -40,6 +44,10 @@ function handleSite() {
             })
         }
         return false;
+    }
+
+    function addKeyToSequence(key) {
+        mySequence.push(key);
     }
 
     function resetSequence() {
@@ -57,5 +65,21 @@ function handleSite() {
     function setIconAsBad(icon) {
         icon.classList.remove("good");
         icon.classList.add("bad");
+    }
+
+    function updateMessage(message) {
+        document.querySelector(".message").textContent = message;
+    }
+
+    function updateTip(tip) {
+        document.querySelector(".tip").textContent = tip;
+    }
+
+    function success() {
+        clearTimeout(outOfTime); //stop reseting after success
+        document.querySelector(".wrapper").classList.add("immortal"); //to trigger blinking animation
+        updateMessage("Udało się! Jesteś nieśmiertelny!");
+        updateTip("Byłeś bardzo szybki.");
+        window.addEventListener("keydown", setInitialState); //to prevent piling-up listeners
     }
 }
